46df20eb75f718ccf69b16f20936f5b9129ab28b
小樽端倶(AxolotlProject) <otarutrpg@gmail.com>
2026-03-28 15:36:10 +0900

    js sleep

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var kaset_mode = "";
-
+var fin = "false";
 
 
 window.onload = loops();
 
-function loops(){
+const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));//timeはミリ秒
+
+async function loops(){
     while(fin != "true"){
         if(kaset_mode == "read"){
             document.getElementById("loading").style.transform = "rotate(" + 10 + "deg)";
         }else if(kaset_mode == "fin"){
             document.getElementById("loading").src = "fin.png";
         }
+        await sleep(100);
     }
 }
