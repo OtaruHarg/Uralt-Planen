1d0f1b64875528df9ef923a5c9b8b43bf7fab859
小樽端倶(AxolotlProject) <otarutrpg@gmail.com>
2026-03-28 21:32:42 +0900

    uuu

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var change = "minus";
 window.onload = start();
 
 async function start(){
-    document.getElementById("gamen_start").style.zIndex = 500;
-    document.getElementById("start_button").style.zIndex = 1000;
+    document.getElementById("gamen_start").style.zIndex = 1000;
+    document.getElementById("start_button").style.zIndex = 1500;
     const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
     console.log("start");
     while(waiting == "true"){
