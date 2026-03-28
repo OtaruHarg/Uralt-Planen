1e7593e8c035d3d8340d4e9dcef2529fdb41bcd3
小樽端倶(AxolotlProject) <otarutrpg@gmail.com>
2026-03-28 20:29:31 +0900

    a

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var kaset_mode = "read";
 var fin = "false";
 var sleeptime = 0;
 let currentRotation = 0;
+var mode_revel = 0;
 
 window.onload = loops();
 
@@ -10,6 +11,25 @@ async function loops(){
     const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
     while(fin != "true"){
         if(kaset_mode == "read"){
+            if(mode_revel == 1){
+                document.getElementById("loading").textContent = ".. LADEN";
+            }else if(mode_revel == 2){
+                document.getElementById("loading").textContent = ". LADEN.";
+            }else if(mode_revel == 3){
+                document.getElementById("loading").textContent = " LADEN..";
+            }else if(mode_revel == 4){
+                document.getElementById("loading").textContent = "LADEN...";
+            }else if(mode_revel == 5){
+                document.getElementById("loading").textContent = "ADEN... ";
+            }else if(mode_revel == 5){
+                document.getElementById("loading").textContent = "DEN... L";
+            }else if(mode_revel == 5){
+                document.getElementById("loading").textContent = "EN... LA";
+            }else if(mode_revel == 5){
+                document.getElementById("loading").textContent = "N... LAD";
+            }else if(mode_revel == 5){
+                document.getElementById("loading").textContent = "... LADE";
+            }
         }else if(kaset_mode == "fin"){
             document.getElementById("loading").src = "fin.png";
         }
