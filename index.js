c8a28c10e1f392719ff596deee2ccb56ad26b746
小樽端倶(AxolotlProject) <otarutrpg@gmail.com>
2026-03-28 18:17:06 +0900

    a

--- index.js
+++ index.js
@@ -10,14 +10,10 @@ async function loops(){
     const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
     while(fin != "true"){
         if(kaset_mode == "read"){
-            const lding = document.getElementById('loading');
-            currentRotation += 10;
-            console.log("trun");
-            lding.style.transform = `rotate(${currentRotation}deg)`;
         }else if(kaset_mode == "fin"){
             document.getElementById("loading").src = "fin.png";
         }
-        await sleep(100);
+        await sleep(500);
         sleeptime = sleeptime + 1;
         if(sleeptime >= 10){
             console.log("time");
