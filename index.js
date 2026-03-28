4fa5f802f3a8be9c042df5de7bc9473a941905bc
小樽端倶(AxolotlProject) <otarutrpg@gmail.com>
2026-03-28 15:28:04 +0900

    js

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ var kaset_mode = "";
 window.onload = loops();
 
 function loops(){
-    if(kaset_mode == "read"){
-        document.getElementById("loading").style.transform = "rotate(" + 10 + "deg)";
-    }else if(kaset_mode == "fin"){
-        document.getElementById("loading").src = "fin.png";
+    while(fin != "true"){
+        if(kaset_mode == "read"){
+            document.getElementById("loading").style.transform = "rotate(" + 10 + "deg)";
+        }else if(kaset_mode == "fin"){
+            document.getElementById("loading").src = "fin.png";
+        }
     }
 }
